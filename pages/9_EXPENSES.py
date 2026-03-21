d66f68af9ddab58237a81014d71efece4a4891e4
peakmyads <vaishali.peakmyads@gmail.com>
2026-03-21 13:41:06 +0530

    Update 9_EXPENSES.py

--- pages/9_EXPENSES.py
+++ pages/9_EXPENSES.py
@@ -7,7 +7,10 @@ from services.data_loader import load_data
 from services.ui_style import apply_sidebar_style
 apply_sidebar_style()
 
-st.set_page_config(layout="wide")
+st.set_page_config(
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
 
 st.title("📊 Expense Tracking")
 
